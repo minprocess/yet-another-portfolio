a0ab1c90af9076243a154a94da4abf8bd8e6ef25
Bill Pate <wtpate69@hotmail.com>
2021-06-27 21:51:35 -0400

    add all of the images

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # yet-another-portfolio
+
+
+## Description
+Portfolio of the web pages I programmed myself or as part of a team. I used MaterializeCSS for this page. It is supposed to be mobile responsive but there is some fixup needed.
+
+## Installation
+This web page has a single html file, a single css file and a single js file. MaterialCSS and jQuery are linked with the CDN website, <cdnjs> (https://cdnjs.com). No APIs are used.
+<br>
+<br>
+Code: https://minprocess.github.io/08-Portfolio/
+<br>
+Page: https://github.com/minprocess/08-Portfolio
+
+## Usage
+![Screen capture of my portfolio page](./assets/images/08-Portfolio-screenshot.png)
+<br>
+
+## Credits
+
+
+## License
+Copyright (c) [2021] [William T Pate]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Tests
+Click on links. Do they work?<br>
+Click on link to web page on a phone. Does the menu work?<br>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-
+// Array of js objects with properties of my web projects so far (Jun 27 2021)
 var projObj = [
   {
     projTitle: "Wobal - a social media site",
     gitHubPages: "",
-    gitHubRepo:  "https://github.com/minprocess/Wobal",
+    gitHubRepo: "https://github.com/minprocess/Wobal",
     herokuApp: "https://wobal-2542.herokuapp.com/",
-    imageName:  "wobal-feed.png",
+    imageName: "wobal-feed.png",
     desc: "A social media site that rhymes with global",
     tech: "Node, bcrypt, connect-session-sequelize, dotenv, eslint, express, express-handlebars, express-session, mysql2, nodemon, sequelize, “badwords” (npm package)"
   },
   {
     projTitle: "Tech Blog",
     gitHubPages: "",
-    gitHubRepo:  "https://github.com/minprocess/14-Tech-Blog",
+    gitHubRepo: "https://github.com/minprocess/14-Tech-Blog",
     herokuApp: "https://tech-blog-2542.herokuapp.com/",
-    imageName:  "",
+    imageName: "14-homepage-tech-blog.png",
     desc: "A full stack website hosting a blog (Tech Talk) allowing users to post articles and to post comments about articles",
     tech: "Node, bcrypt, connect-session-sequelize, dotenv, eslint, express, express-handlebars, express-session, mysql2, sequelize"
   },
@@ -23,7 +23,7 @@ var projObj = [
     gitHubPages: "",
     gitHubRepo: "https://github.com/minprocess/13-E-Commerce-Backend",
     herokuApp: "https://ecomm-backend-2542.herokuapp.com/",
-    imageName: "",
+    imageName: "13-Insomnia_requests_for_backend_on_Heroku.png",
     desc: "Backend for an e-commerce website",
     tech: "Noed, Javascript, dotenv, express, mysql2, sequelize"
   },
@@ -32,7 +32,7 @@ var projObj = [
     gitHubPages: "",
     gitHubRepo: "https://github.com/minprocess/12-Employee-Tracker",
     herokuApp: "",
-    imageName:  "12-Main_menu_of_Employee_Tracker.png",
+    imageName: "12-Main_menu_of_Employee_Tracker.png",
     desc: "A command-line application that allows the user to interact with a mySQL database containing three tables of employee information.",
     tech: "Node, JavaScript, console.table, inquirer, mysql"
   },
@@ -50,34 +50,34 @@ var projObj = [
     gitHubPages: "",
     gitHubRepo: "https://github.com/minprocess/10-Team-Profile-Generator",
     herokuApp: "",
-    imageName:  "10-screencapture-team.html.png",
+    imageName: "10-Team-profile-screen-capture.png",
     desc: "This app is a Node.js command-line application that a user questions about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.",
     tech: "HTML, CSS, Node, Javascript, Inquirer"
   },
   {
     projTitle: "Readme Autogen",
-    gitHubPages: "https://minprocess.github.io/09-Readme-Autogen/",
-    gitHubRepo:  "https://github.com/minprocess/09-Readme-Autogen",
+    gitHubPages: "",
+    gitHubRepo: "https://github.com/minprocess/09-Readme-Autogen",
     herokuApp: "",
-    imageName:  "09-readme-autogen.png",
+    imageName: "09-readme-autogen.png",
     desc: "A JavaScript app uses Inquirer.js to ask a programmer a series of questions and based on that generate a readme file.",
     tech: "Javascript, Inquirer.js"
   },
   {
     projTitle: "Open Road Travel Agency",
     gitHubPages: "https://minprocess.github.io/Open-Road-Travel-Agency",
-    gitHubRepo:  "https://github.com/minprocess/Open-Road-Travel-Agency",
+    gitHubRepo: "https://github.com/minprocess/Open-Road-Travel-Agency",
     herokuApp: "",
-    imageName:  "Open-Road-Travel-Agency",
+    imageName: "Open-Road-Travel-Agency.png",
     desc: "Open Road Travel Agency website with a little bit of Covid-19 info. MVP obviously!",
     tech: "HTML, CSS, Javascript, 3 APIs "
   },
   {
     projTitle: "Simple Weather",
     gitHubPages: "https://minprocess.github.io/06-Simple-Weather",
-    gitHubRepo:  "https://github.com/minprocess/06-Simple-Weather",
+    gitHubRepo: "https://github.com/minprocess/06-Simple-Weather",
     herokuApp: "",
-    imageName:  "06-Simple-Weather",
+    imageName: "06-Simple-Weather.png",
     desc: "Weather app with current temperature and 5 day forecast. Uses OpenWeatherMap API",
     tech: "HTML, CSS, Javascript, jQuery, Moment.js, 1 API"
   },
@@ -86,125 +86,53 @@ var projObj = [
     gitHubPages: "https://minprocess.github.io/05-Calendar",
     gitHubRepo:  "https://github.com/minprocess/05-Calendar",
     herokuApp: "",
-    imageName:  "05-Calendar",
+    imageName:  "05-Calendar.png",
     desc: "Simple daily planner. 'Events' entered by the user are kept in local storage until the next day",
     tech: "HTML, CSS, JavaScript, jQuery, Moment.js"
   },
   {
     projTitle: "Quiz",
     gitHubPages: "https://minprocess.github.io/04-Quiz",
-    gitHubRepo:  "https://github.com/minprocess/04-Quiz",
+    gitHubRepo: "https://github.com/minprocess/04-Quiz",
     herokuApp: "",
-    imageName:  "04-Quiz",
+    imageName: "04-Quiz.png",
     desc: "Quiz about JavaScript. Has a timer. Score is calculated from the time left on timer.",
     tech: "HTML, CSS, JavaScript"
   },
   {
     projTitle: "Password Generator",
     gitHubPages: "https://minprocess.github.io/03-SecPassGen",
-    gitHubRepo:  "https://github.com/minprocess/03-SecPassGen",
+    gitHubRepo: "https://github.com/minprocess/03-SecPassGen",
     herokuApp: "",
-    imageName:  "03-SecPassGen",
+    imageName: "03-SecPassGen.png",
     desc: "Generate a password containing random characters. Can be customized.",
     tech: "HTML, CSS, JavaScript"
   },
-
   {
     projTitle: "Horiseon Refector",
     gitHubPages: "https://minprocess.github.io/01-Horiseon-Refactor",
-    gitHubRepo:  "https://github.com/minprocess/01-Horiseon-Refactor",
+    gitHubRepo: "https://github.com/minprocess/01-Horiseon-Refactor",
     herokuApp: "",
-    imageName: "01-Horiseon-Refactor.jpg",
+    imageName: "01-Horiseon-Refactor.png",
     desc: "Refactor a website for social media and SEO company.",
     tech: "HTML, CSS"
-  },
-
-
+  }
 ]
 
-
-var projNames = [
-    "Horiseon Refactor",
-    "Password Generator",
-    "Quiz",
-    "Planner",
-    "Simple Weather",
-    "Open Road Travel Agency"
-  ]
-
-  var pageNames = [
-    "https://minprocess.github.io/01-Horiseon-Refactor",
-    "https://minprocess.github.io/03-SecPassGen",
-    "https://minprocess.github.io/04-Quiz",
-    "https://minprocess.github.io/05-Calendar",
-    "https://minprocess.github.io/06-Simple-Weather",
-    "https://amysmerlick.github.io/Open-Road-Travel-Agency"
-  ]
-  
-  var repoNames = [
-    "https://github.com/minprocess/01-Horiseon-Refactor",
-    "https://github.com/minprocess/03-SecPassGen",
-    "https://github.com/minprocess/04-Quiz",
-    "https://github.com/minprocess/05-Calendar",
-    "https://github.com/minprocess/06-Simple-Weather",
-    "https://github.com/amysmerlick/Open-Road-Travel-Agency"
-  ]
-
-  var imgNames = [
-      "01-Horiseon-Refactor",
-      "03-SecPassGen",
-      "04-Quiz",
-      "05-Calendar",
-      "06-Simple-Weather",
-      "Open-Road-Travel-Agency"
-  ]
-
-  const projDesc = [
-    "Refactor a website for social media and SEO company.",
-    "Generate a password containing random characters. Can be customized.",
-    "Quiz about JavaScript. Has a timer. Score is calculated from the time left on timer.",
-    "Simple daily planner. 'Events' entered by the user are kept in local storage until the next day",
-    "Weather app with current temperature and 5 day forecast. Uses OpenWeatherMap API",
-    "Open Road Travel Agency website with a little bit of Covid-19 info. MVP obviously!"
-  ]
-
-
+// Wait until css and scripts are loaded before loading the html for the projects
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
     
     var projList = document.querySelector("#proj-list");  // ul
     var html = "";
-    for (var i=0; i<projNames.length; i++) {
+    for (var i=0; i<projObj.length; i++) {
         html += cardHtml(i)
     }
     projList.innerHTML = html;
   });
 
-  cardHtml2 = function (i) {
-    var html;
-    html = `<div class="col s12 l4">
-    <div class="card">
-      <div class="card-image">
-        <a href="${pageNames[i]}"> <img src="./assets/images/${imgNames[i]}.png" object-fit w3shool></a>
-      </div>
-      <div class="card-content">
-        <span class="card-title">${projNames[i]}</span>
-        <p>${projDesc[i]}</p>
-      </div>
-      <div class="card-action">
-        <a href="${repoNames[i]}">Code</a>
-        <a href="${pageNames[i]}">Page</a>
-      </div>
-    </div>
-  </div>
-</div>
-`
-    return html;
-
-  }
-
-  // https://www.w3schools.com/csS/css3_object-fit.asp
+ 
 cardHtml = function (i) {
     var html;
     let pageName = "";
@@ -219,7 +147,6 @@ cardHtml = function (i) {
     if (projObj[i].imageName != "") {
       imgName = projObj[i].imageName;
     }
-    console.log("img name", imgName)
 
     let title = projObj[i].projTitle;
 
@@ -246,30 +173,3 @@ cardHtml = function (i) {
 `
     return html;
 }
-/*
-  <!-- 01-Horiseon Refactor -->
-  <div class="col s3 l3">
-    <div class="card">
-      <div class="card-image">
-        <img src="./assets/images/proj-1-Horiseon.jpg">
-      </div>
-      <div class="card-content">
-        <span class="card-title">Horiseon Refactor</span>
-        <p>Refactor a website for social media and SEO company.</p>
-      </div>
-      <div class="card-action">
-        <a href="https://github.com/minprocess/01-Horiseon-Refactor">Code</a>
-        <a href="https://minprocess.github.io/01-Horiseon-Refactor/">Page</a>
-      </div>
-    </div>
-  </div>
-</div>
-*/
-
-
-/*
-  // Or with jQuery
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-*/
