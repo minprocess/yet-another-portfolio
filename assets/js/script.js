cc3aca5ee50ed6b645a56fa0bac04471fb41357c
Bill Pate <wtpate69@hotmail.com>
2021-07-20 22:48:39 -0400

    add altText to the image for each project

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ var projObj = [
     imageName: "18-Snapshot-homepage-workout-tracker.png",
     desc: "Workout Tracker is a full stack web app to view, create and track daily workouts. Used starter code.",
     tech: "Node, Express, Mongoose and JavaScript for the backend. HTML, CSS and Javascript for the frontend.",
+    altText: "Home page of Workout Tracker app"
   },
   {
     projTitle: "Using a regex for validating a password string",
@@ -15,6 +16,7 @@ var projObj = [
     imageName: "17-screenshot-using-a-regex-validating.png",
     desc: "A Gist explaining a regular expression that can be used to validate a password.",
     tech: "Gist on GitHub, Markdown",
+    altText: "Gist article explaining a regex for validating a password string"
   },
   {
     projTitle: "Wobal - a social media site",
@@ -23,6 +25,7 @@ var projObj = [
     imageName: "wobal-feed.png",
     desc: "A full stack social media web site that rhymes with global. Simple, easy to use and won't sell data about you. Team project",
     tech: "Node, bcrypt, connect-session-sequelize, dotenv, eslint, Express, express-handlebars, express-session, MySQL2, nodemon, Sequelize.js, “badwords” (a npm package)",
+    altText: "Home page of the Wobal social media app"
   },
   {
     projTitle: "Tech Blog",
@@ -31,6 +34,7 @@ var projObj = [
     imageName: "14-homepage-tech-blog.png",
     desc: "A full stack website hosting a blog (The Tech Blog) allowing users to post articles and to post comments about articles.",
     tech: "Node, bcrypt, connect-session-sequelize, dotenv, eslint, express, express-handlebars, express-session, MySQL2, Sequelize.js",
+    altText: "Home page of the Tech Blog app"
   },
   {
     projTitle: "E-commerce-backend",
@@ -39,52 +43,52 @@ var projObj = [
     imageName: "13-Insomnia_requests_for_backend_on_Heroku.png",
     desc: "Backend for an e-commerce website. Insomnia is used to test the CRUD actions. Used starter code",
     tech: "Node, Javascript, dotenv, express, MySQL2, Sequelize.js",
+    altText: "Insomnia app showing transactions used with the backend"
   },
   {
     projTitle: "Employee Tracker",
     gitHubRepo: "https://github.com/minprocess/12-Employee-Tracker",
     deployLink: "",
     imageName: "12-Main_menu_of_Employee_Tracker.png",
-    desc: "A command-line application that allows the user to interact with a MySQL2 database containing three tables of employee information.",
+    desc: "A command-line application that allows the user to interact with a MySQL database containing three tables of employee information.",
     tech: "Node, JavaScript, console.table, Inquirer.js, MySQL2",
+    altText: "Snapshot of interface to a MySQL database made with Inquirer.js"
   },
   {
     projTitle: "Note Taker",
-    gitHubPages: "",
     gitHubRepo: "https://github.com/minprocess/11-Note-Taker",
-    herokuApp: "https://note-taker-wtp.herokuapp.com/",
+    deployLink: "https://note-taker-wtp.herokuapp.com/",
     imageName: "11-screen-capture-note-taker.png",
-    desc: "Web site to write and save notes. All notes are saved in one database. Homework for UPenn Coding Bootcamp",
+    desc: "Web site to write and save notes. All notes are saved in one database. Used starter code",
     tech: "HTML, CSS, Node, Javascript, Express, uuidv4",
-    myRole: "contributer"
+    altText: "Home page of Note Taker app showing list of stored notes"
   },
   {
     projTitle: "Team profile generator",
-    gitHubPages: "",
     gitHubRepo: "https://github.com/minprocess/10-Team-Profile-Generator",
-    herokuApp: "",
+    deployLink: "",
     imageName: "10-Team-profile-screen-capture.png",
-    desc: "This app is a Node.js command-line application that a user questions about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person. Homework for UPenn Coding Bootcamp",
+    desc: "This app is a Node.js command-line application that a user questions about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person. Used starter code",
     tech: "HTML, CSS, Node, Javascript, Inquirer.js",
-    myRole: "Sole programmer"
+    altText: "Homepage of this app which was created after answering questions in Inquirer.js"
   },
   {
     projTitle: "Readme Autogen",
-    gitHubPages: "",
     gitHubRepo: "https://github.com/minprocess/09-Readme-Autogen",
-    herokuApp: "",
+    deployLink: "",
     imageName: "09-readme-autogen.png",
     desc: "A JavaScript app uses Inquirer.js to ask a programmer a series of questions and based on that generate a readme file.",
     tech: "Javascript, Inquirer.js",
-    myRole: "Sole programmer"
+    altText: "Snapshot of the readme created with this app that uses Inquirer.js"
   },
   {
     projTitle: "Open Road Travel Agency",
-    gitHubPages: "https://minprocess.github.io/Open-Road-Travel-Agency",
     gitHubRepo: "https://github.com/minprocess/Open-Road-Travel-Agency",
+    deployLink: "https://minprocess.github.io/Open-Road-Travel-Agency",
     imageName: "Open-Road-Travel-Agency.png",
     desc: "Open Road Travel Agency website with a little bit of Covid-19 info. MVP. Group project",
     tech: "HTML, CSS, Javascript, 3 APIs ",
+    altText: "Homepage of this travel agency app"
   },
   {
     projTitle: "Simple Weather",
@@ -93,6 +97,7 @@ var projObj = [
     imageName: "06-Simple-Weather.png",
     desc: "Weather app with current temperature and 5 day forecast. Uses OpenWeatherMap API",
     tech: "HTML, CSS, Javascript, jQuery, Moment.js, 1 API",
+    altText: "Homepage of this weather app"
   },
   {
     projTitle: "Planner",
@@ -101,6 +106,7 @@ var projObj = [
     imageName:  "05-Calendar.png",
     desc: "Simple daily planner. 'Events' entered by the user are kept in local storage until the next day. Used starter code.",
     tech: "HTML, CSS, JavaScript, jQuery, Moment.js",
+    altText: "Homepage if this simple day planner type of app"
   },
   {
     projTitle: "Quiz",
@@ -109,6 +115,7 @@ var projObj = [
     imageName: "04-Quiz.png",
     desc: "Quiz about JavaScript. Score is calculated from the time left on timer.",
     tech: "HTML, CSS, JavaScript",
+    altText: "Homepage of this app which asks 5 questions about JavaScript"
   },
   {
     projTitle: "Password Generator",
@@ -116,7 +123,8 @@ var projObj = [
     deployLink: "https://minprocess.github.io/03-SecPassGen",
     imageName: "03-SecPassGen.png",
     desc: "Generate a password containing random characters. Password can be customized. Used starter code",
-    tech: "HTML, CSS, JavaScript"
+    tech: "HTML, CSS, JavaScript",
+    altText: "Homepage of this app which generates a random password given parameters by user"
   },
   {
     projTitle: "Horiseon Refector",
@@ -124,7 +132,8 @@ var projObj = [
     deployLink: "https://minprocess.github.io/01-Horiseon-Refactor",
     imageName: "01-Horiseon-Refactor.png",
     desc: "Refactor a website for social media and SEO company. Used starter code.",
-    tech: "HTML, CSS"
+    tech: "HTML, CSS",
+    altText: "Homepage of a website for social media and SEO company"
   }
 ]
 
@@ -166,19 +175,6 @@ document.addEventListener('DOMContentLoaded', function() {
  
 cardHtml = function (i) {
     var html;
-    /*
-    let pageName = "";
-    if (projObj[i].gitHubPages != "") {
-      pageName = projObj[i].gitHubPages
-    }
-    else if (projObj[i].herokuApp != "") {
-      pageName = projObj[i].herokuApp;
-    }
-    */
-    //<a href="${pageName}"> <img src="./assets/images/${imgName}" object-fit w3shool></a>
-
-    //let deployLink = projObj[i].deployLink;
-    //<a href="${pageName}">Page</a>
     let deployLink = "";
     if (projObj[i].deployLink != "")
       deployLink = '<a href="'+projObj[i].deployLink + '">Page</a>';
@@ -187,10 +183,12 @@ cardHtml = function (i) {
     if (projObj[i].imageName != "") {
       imgName = projObj[i].imageName;
     }
-    let title = projObj[i].projTitle;
-    let desc = projObj[i].desc;
-    let tech = projObj[i].tech;
-    let githubRepo = projObj[i].gitHubRepo;
+    const title = projObj[i].projTitle;
+    const desc = projObj[i].desc;
+    const tech = projObj[i].tech;
+    const githubRepo = projObj[i].gitHubRepo;
+    const altText = projObj[i].altText;
+
 /*
     if (pageName=='') {
       set-attribute=hide
@@ -201,16 +199,12 @@ cardHtml = function (i) {
       json for web projects
       json for publications c++, c#
     }
-
-    fix
-
-    see object-fit in w3school
 */
     html = `
     <div class="col s12 l4">
       <div class="card">
         <div class="card-image">
-          <img src="./assets/images/${imgName}">
+          <img src="./assets/images/${imgName}" alt=${altText}>
         </div>
         <div class="card-content" style="min-height: 255px">
           <span class="card-title">${title}</span>
